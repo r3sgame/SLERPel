da848f65d228665a123615435088dcac6c3ab84f
r3sgame <68745659+r3sgame@users.noreply.github.com>
2022-11-03 20:42:53 -0400

    l;k;;;;

--- react-app/src/Game/GameWindow.js
+++ react-app/src/Game/GameWindow.js
@@ -4,16 +4,18 @@ import { IonPhaser } from '@ion-phaser/react'
 
 //Define GameObjects
 var player;
+var bot;
 var platforms;
+var goal;
 
 //Define Systems
 var cursors;
 
 //Velocity
 var xVelocity = 0;
-var speedCap = 400;
-var speedMultiplier = 5;
-var deceleration = 0.1;
+var speedCap = 15;
+var speedMultiplier = 1;
+var deceleration = 0.25;
 
 //Initialize Tilemap
 var tilemap = new Array(10);
@@ -23,7 +25,7 @@ for (var i = 0; i < tilemap.length; i++) {
 
 //Level Editor
 tilemap[0] = [0,0,0,0,0,0,0,0,0,0];
-tilemap[1] = [0,0,0,0,0,0,0,0,0,0];
+tilemap[1] = [0,0,0,0,0,0,0,0,2,0];
 tilemap[2] = [0,0,0,0,0,0,0,0,1,0];
 tilemap[3] = [0,0,0,0,0,0,0,0,0,0];
 tilemap[4] = [0,0,0,0,0,1,0,0,0,0];
@@ -43,7 +45,7 @@ class GameWindow extends Component {
       physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 1100 },
+            gravity: { y: 0 },
             debug: false
         }
     },
@@ -51,16 +53,21 @@ class GameWindow extends Component {
         preload: function() {
           this.load.image('player', 'assets/Player.png');
           this.load.image('tile', 'assets/Tile.png');
+          this.load.image('bot', 'assets/Bot.png');
+          this.load.image('goal', 'assets/Goal.png');
         },
         init: function() {
           this.cameras.main.setBackgroundColor('#000000');
-          
         },
         create: function() {
           
           //Player
           player = this.physics.add.sprite(100, 450, 'player');
           player.setCollideWorldBounds(true);
+          
+          //Bot
+          bot = this.physics.add.sprite(100, 450, 'bot');
+          bot.setCollideWorldBounds(true);
 
           //Render Tilemap
           platforms = this.physics.add.staticGroup();
@@ -70,17 +77,26 @@ class GameWindow extends Component {
               
               if(tilemap[yIndex][xIndex] === 1)
               platforms.create(30+(60*xIndex), 30+(60*yIndex), 'tile');
-    
+
+              if(tilemap[yIndex][xIndex] === 2) {
+              goal = this.physics.add.staticSprite(30+(60*xIndex), 30+(60*yIndex), 'goal');
+              this.physics.add.overlap(player, goal);
+              this.physics.add.overlap(bot, goal);
+              }
               }
           }
 
           //Physics
           this.physics.add.collider(player, platforms);
+          this.physics.add.collider(bot, platforms);
 
           //Input
           cursors = this.input.keyboard.createCursorKeys();
         },
-        update: function() {
+        update: function(time, delta) {
+          player.setGravity(0, 50*delta);
+          bot.setGravity(0, 50*delta);
+
           //Player
           if (cursors.left.isDown)
           {
@@ -108,10 +124,10 @@ class GameWindow extends Component {
 
           if (cursors.up.isDown && player.body.touching.down)
           {
-            player.setVelocityY(-600);
+            player.setVelocityY(-30*delta);
           }
           
-          player.setVelocityX(xVelocity);
+          player.setVelocityX(xVelocity*delta);
         }
       }
     }
